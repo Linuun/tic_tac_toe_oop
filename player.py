42164909e7a0ad1af22b0cd7e1c6983debc1fcd6
Linuun <jlenonninolla31@gmail.com>
2025-06-24 14:44:36 +0800

    added another child class called RandomComputerPlayer

--- player.py
+++ player.py
@@ -25,3 +25,11 @@ class HumanPlayer(Player):
             except ValueError:
                 print('Invalid square. Try again.')
         return val
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
