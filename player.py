c5cb8cf361ea2ef65e8aadc4c994de8bde83eecb
Linuun <jlenonninolla31@gmail.com>
2025-06-24 14:42:35 +0800

    added the first child class called HumanPlayer

--- player.py
+++ player.py
@@ -7,3 +7,21 @@ class Player():
 
     def get_move(self, game):
         pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9): ')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again.')
+        return val
