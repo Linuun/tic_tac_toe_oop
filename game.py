6f4ea112c292d528e8cb0110df8bce66a76a9dd9
Linuun <jlenonninolla31@gmail.com>
2025-06-24 15:05:29 +0800

    make an object calling the necesarry classes to run the code

--- game.py
+++ game.py
@@ -90,3 +90,9 @@ def play(game, x_player, o_player, print_game=True):
 
     if print_game:
         print('It\'s a tie!')
+
+if __name__ == '__main__':
+    x_player = SmartComputerPlayer('X')
+    o_player = HumanPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
